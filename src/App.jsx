32e82b98da5978141de61f0b0bd969e173ef32cb
Qasim Rokeeb <qasimrokeeb345@gmail.com>
2025-05-07 13:05:57 +0100

    kjjk

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 // src/App.jsx
-import { sdk } from '@farcaster/frame-sdk';
-await sdk.actions.ready();
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
@@ -18,4 +17,4 @@ function App() {
 }
 
 export default App;
-await sdk.actions.ready({ disableNativeGestures: true });
+
